592c74e959f0e29a1938b6bc5e6f453b75277b1f
James Lawrence Turner <jlturner@fastmail.com>
2020-02-12 10:11:44 -0800

    using dynamic

--- pages/admin.js
+++ pages/admin.js
@@ -1,21 +1,26 @@
-import CMS from 'netlify-cms-app'
-import Homepage from "../components/Homepage";
+import dynamic from 'next/dynamic';
 
-function handleChange(entry, f) {
-  const data = entry.getIn(['data']).toJS();
-  return data ?
-    (f(data)) :
-    (<div>Loading...</div>); // TODO: make this a spinner!
-}
+const CMSDynamic = dynamic(() => {
+  import CMS from 'netlify-cms-app'
+  import Homepage from "../components/Homepage";
 
-CMS.registerPreviewTemplate('index', ({ entry, getAsset }) => {
-  handleChange(entry, data => (<Homepage title={data.title} cats={data.cats} />));
+  function handleChange(entry, f) {
+    const data = entry.getIn(['data']).toJS();
+    return data ?
+      (f(data)) :
+      (<div>Loading...</div>); // TODO: make this a spinner!
+  }
+
+  CMS.registerPreviewTemplate('index', ({ entry, getAsset }) => {
+    handleChange(entry, data => (<Homepage title={data.title} cats={data.cats} />));
+  })
 })
-
+  
 export default () => {
   return(<>
     <Head>
       <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      <CMSDynamic />
     </Head>
   </>);
 };
