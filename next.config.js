3a52c2618fe42f08dc5316448487548b9af56d4f
James Lawrence Turner <jlturner@fastmail.com>
2020-02-12 16:20:33 -0800

    componentDidMount

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-const path = require("path");
 const withSass = require('@zeit/next-sass');
 
 module.exports = 
   withSass(
     {
       webpack: (cfg) => {
-          cfg.resolve = cfg.resolve || {};
-          cfg.resolve.alias = cfg.resolve.alias || {};
-          cfg.resolve.alias['~'] = path.join(__dirname, 'components');
-          cfg.resolve.alias['node_modules'] = path.join(__dirname, 'node_modules');
-
           cfg.module.rules.push(
               {
                   test: /\.md$/,
--- pages/cms.js
+++ pages/cms.js
@@ -1,7 +1,26 @@
-import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-const CMSDynamic = dynamic(() => import('../components/CMSPage'), { ssr: false });
+class CMSPostLoad extends React.Component {
+  render() {
+    return <input type="hidden" />
+  }
+
+  componentDidMount() {
+    if(typeof window !== undefined) {
+      function handleChange(entry, f) {
+        const data = entry.getIn(['data']).toJS();
+        return data ?
+          (f(data)) :
+          (<div>Loading...</div>); // TODO: make this a spinner!
+      }
+    
+      // Page Preview Bindings
+      window.CMS.registerPreviewTemplate('index', ({ entry, getAsset, widgetFor }) => {
+        handleChange(entry, data => (<Homepage title={data.title} cats={data.cats}>{widgetFor('body')}</Homepage>));
+      });
+    }
+  }
+}
 
 export default () => {
   return(<>
@@ -9,6 +28,6 @@ export default () => {
       <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
     </Head>
     <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-    <CMSDynamic />
+    <CMSPostLoad />
   </>);
 };
